6a5dbc1d23ee0dc0a49d8888a479ff515a8e1b51
Kyle Norris <r.kyle.norris@gmail.com>
2025-09-04 18:12:56 -0400

    copied sql cmds module from prototype

---
+++ sql_cmds/__init__.py
@@ -0,0 +1,1 @@
+from .sql_cmds import create_db_conn, read_sql_view_to_df, execute_sql_command, execute_sql_script
---
+++ sql_cmds/sql_cmds.py
@@ -0,0 +1,53 @@
+import sqlite3
+from pathlib import Path
+import pandas as pd
+from log_setup import logger
+
+
+def create_db_conn(db_path) -> sqlite3.Connection:
+    """
+    creates a new database connection to the SQLite database
+    :return:
+    """
+    return sqlite3.connect(db_path)
+
+
+def execute_sql_command(conn: sqlite3.Connection, command: str, commit: bool = True, *args):
+    with conn:
+        cursor = conn.cursor()
+        if args:
+            if len(*args) == 1:
+                cursor.execute(command, args)
+            else:
+                cursor.execute(command, *args)
+        else:
+            cursor.execute(command)
+
+        if commit:
+            conn.commit()
+        else:
+            return cursor.fetchall()
+
+
+def execute_sql_script(conn: sqlite3.Connection, script_path: str, commit: bool = True):
+    with conn:
+        script = Path(script_path)
+        if not script.exists():
+            logger.error(f"SQL script {script_path} does not exist.")
+            return
+        logger.info(f"Executing script: {script.name}")
+        cursor = conn.cursor()
+        script_text = script.read_text()
+        cursor.executescript(script_text)
+        if commit:
+            conn.commit()
+        else:
+            return cursor.fetchall()
+
+
+def read_sql_view_to_df(conn: sqlite3.Connection, view_name: str) -> pd.DataFrame:
+    logger.info(f"Retrieving data from view {view_name}...")
+    query = f"SELECT * FROM {view_name}"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
