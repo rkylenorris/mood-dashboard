6b43f3205120a86d91a912bd6563db1844de92ed
Kyle Norris <r.kyle.norris@gmail.com>
2025-09-07 18:51:10 -0400

    troubleshooting adding topics issue

--- sql_cmds/sql_cmds.py
+++ sql_cmds/sql_cmds.py
@@ -22,11 +22,10 @@ def execute_sql_command(conn: sqlite3.Connection, command: str, commit: bool = T
     with conn:
         cursor = conn.cursor()
         if args:
-            cursor.execute(command, *args)
-            # if len(args) == 1:
-            #     cursor.execute(command, args)
-            # else:
-            #     cursor.execute(command, *args)
+            if len(*args) == 1:
+                cursor.execute(command, args)
+            else:
+                cursor.execute(command, *args)
         else:
             cursor.execute(command)
 
